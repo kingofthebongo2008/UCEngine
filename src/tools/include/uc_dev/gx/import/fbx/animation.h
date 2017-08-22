0846f4cf1243f0c04c309ee0be0f4dd4725adf07
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2017-08-22 16:17:03 +0300

    added skeleton for animations

---
+++ src/tools/include/uc_dev/gx/import/fbx/animation.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+#include <uc_dev/gx/import/anm/animation.h>
+
+namespace uc
+{
+    namespace gx
+    {
+        namespace import
+        {
+            namespace fbx
+            {
+                inline std::vector<anm::joint_animations> create_animations(const std::string& file_name)
+                {
+                    std::vector<anm::joint_animations> r;
+                    return r;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
