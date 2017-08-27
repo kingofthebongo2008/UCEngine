51f454ff3b7b858bfdbc8090a4f6a4cb0abc1dd7
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2017-08-27 14:02:53 +0300

    added fbx transform skeleton

---
+++ src/tools/include/uc_dev/gx/import/fbx/fbx_transform.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <tuple>
+#include <uc_dev/gx/error.h>
+#include <uc_dev/math/math.h>
+
+#include <fbxsdk.h>
+
+namespace uc
+{
+    namespace gx
+    {
+        namespace import
+        {
+            namespace fbx
+            {
+            }
+        }
+    }
+}
+
+
