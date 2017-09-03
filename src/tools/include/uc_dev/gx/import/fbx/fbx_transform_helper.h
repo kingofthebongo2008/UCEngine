31a0814f118799f7bb3531c07c54fd74e552a35e
lee dorian <leedorian88@abv.bg>
2017-09-03 15:34:17 +0300

    added if, for swapping axes

--- src/tools/include/uc_dev/gx/import/fbx/fbx_transform_helper.h
+++ src/tools/include/uc_dev/gx/import/fbx/fbx_transform_helper.h
@@ -19,31 +19,62 @@ namespace uc
 
                 math::float4x4 transform_from_dcc(math::afloat4x4 m, const fbx_context* ctx)
                 {
-                    auto m0 = swap_y_z();
-                    auto m1 = negate_x();
-                    auto m2 = negate_y();
-                    auto m3 = negate_z();
-                    auto m4 = math::identity_matrix();
+                    if (ctx->m_coordinate_system_swap_y_z)
+                    {
+                        auto m0 = swap_y_z();
+                        auto m1 = negate_x();
+                        auto m2 = negate_y();
+                        auto m3 = negate_z();
+                        auto m4 = math::identity_matrix();
 
-                    return transform_transform(transform_transform(transform_transform(m, m3), m2), m1);
-                    
+                        return transform_transform(transform_transform(transform_transform(m, m3), m2), m1);
+                    }
+                    else
+                    {
+                        auto m0 = swap_y_z();
+                        auto m1 = negate_x();
+                        auto m2 = negate_y();
+                        auto m3 = negate_z();
+                        auto m4 = math::identity_matrix();
+
+                        return transform_transform(transform_transform(transform_transform(m, m3), m2), m1);
+                    }
                 }
 
                 math::float4 transform_from_dcc(math::afloat4 m, const fbx_context* ctx)
                 {
-                    auto m0 = swap_y_z();
-                    auto m1 = negate_x();
-                    auto m2 = negate_y();
-                    auto m3 = negate_z();
-                    auto m4 = math::identity_matrix();
-                    return transform_vector(transform_vector(transform_vector(m, m3), m2), m1);
+                    if (ctx->m_coordinate_system_swap_y_z)
+                    {
+                        auto m0 = swap_y_z();
+                        auto m1 = negate_x();
+                        auto m2 = negate_y();
+                        auto m3 = negate_z();
+                        auto m4 = math::identity_matrix();
+                        return transform_vector(transform_vector(transform_vector(m, m3), m2), m1);
+                    }
+                    else
+                    {
+                        auto m0 = swap_y_z();
+                        auto m1 = negate_x();
+                        auto m2 = negate_y();
+                        auto m3 = negate_z();
+                        auto m4 = math::identity_matrix();
+                        return transform_vector(transform_vector(transform_vector(m, m3), m2), m1);
+                    }
                 }
 
                 std::array<int32_t, 3> triangle_permuation(const fbx_context* ctx)
                 {
-                    ctx;
-                    std::array<int32_t, 3> r = { 0, 1, 2 };
-                    return r;
+                    if (ctx->m_coordinate_system_swap_y_z)
+                    {
+                        std::array<int32_t, 3> r = { 0, 1, 2 };
+                        return r;
+                    }
+                    else
+                    {
+                        std::array<int32_t, 3> r = { 0, 1, 2 };
+                        return r;
+                    }
                 }
 
             }
