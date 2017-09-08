1f37c217e9edf9261917ca8c036a62bd00e06507
lee dorian <leedorian88@abv.bg>
2017-09-08 08:58:08 +0300

    added transform to view space

--- src/uc_dev/private/gx/dx12/shaders/static_geometry/textured_pixel_lit.hlsl
+++ src/uc_dev/private/gx/dx12/shaders/static_geometry/textured_pixel_lit.hlsl
@@ -1,5 +1,7 @@
 #include "../default_signature.hlsli"
 
+#include "frame.hlsli"
+
 struct interpolants
 {
     float4 position       : SV_POSITION0;
@@ -34,11 +36,11 @@ float4 main( interpolants r ) : SV_Target0
 {
     float3 dudx                       = ddx(r.position_ws);
     float3 dudy                       = ddy(r.position_ws);
-    float3 normal_ws                  = normalize(cross(dudx, dudy));
-    float3 sun_light_direction_ws     = normalize(float3(1.0, 0.5, 0.5));
+    float3 normal_vs                  = mul(float4(normalize(cross(dudx, dudy)), 0.0f), m_view).xyz;
+    float3 sun_light_direction_vs     = mul(float4(normalize(float3(1.0, 0.5, 0.5)), 0.0f), m_view).xyz;
     float3 sun_light_intensity        = float3(0.5, 0.5, 0.5);
     float3 albedo                     = float4(1.0f, 1.0f, 1.0f, 1.0f);
-    float  ndotl                      = saturate(dot(normal_ws, sun_light_direction_ws));
+    float  ndotl                      = saturate(dot(normal_vs, sun_light_direction_vs));
     float3 ambient                    = float3(0.2f, 0.2f, 0.2f);
     
     return float4 (ndotl * albedo * sun_light_intensity + ambient, 0.0);
