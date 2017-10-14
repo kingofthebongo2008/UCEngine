5c715e1bd430573604ab8def8d4f1ee5d620fb48
lee dorian <leedorian88@abv.bg>
2017-10-14 19:19:12 +0300

    added simple shadow map

--- src/uc_dev/private/gx/dx12/shaders/static_geometry/textured_pixel_lit.hlsl
+++ src/uc_dev/private/gx/dx12/shaders/static_geometry/textured_pixel_lit.hlsl
@@ -1,4 +1,5 @@
 #include "../default_signature.hlsli"
+#include "../default_samplers.hlsli"
 
 #include "vector_space.hlsli"
 #include "frame.hlsli"
@@ -25,11 +26,17 @@ float4 main( interpolants r ) : SV_Target0
     float3 dudx                       = ddx(r.position_ws);
     float3 dudy                       = ddy(r.position_ws);
     float3 normal_vs                  = mul(float4(normalize(cross(dudx, dudy)), 0.0f), m_view.m_value).xyz;
-    float3 sun_light_direction_vs     = mul(float4(normalize(float3(1.0, 0.5, 0.5)), 0.0f), m_view.m_value).xyz;
+    float3 sun_light_direction_vs     = mul(float4(normalize(float3(0.0, 1.0, 0.0)), 0.0f), m_view.m_value).xyz;
     float3 sun_light_intensity        = float3(0.5, 0.5, 0.5);
     float3 albedo                     = float4(1.0f, 1.0f, 1.0f, 1.0f);
+    float4 light_ps                   = project_p_ws(make_point_ws(r.position_ws), m_shadow_view, m_shadow_perspective).m_value;
+
+    light_ps                          = light_ps / light_ps.w;
+    float  shadow                     = g_shadow_moments.Sample(g_linear_clamp,  light_ps.xy / 2.0f + 0.5f ).x;
+
+    float  shadow_intensity           = shadow > light_ps.z ? 0.0f : 1.0f; 
     float  ndotl                      = saturate(dot(normal_vs, sun_light_direction_vs));
     float3 ambient                    = float3(0.2f, 0.2f, 0.2f);
     
-    return float4 (ndotl * albedo * sun_light_intensity + ambient, 0.0);
+    return float4 (shadow_intensity * ndotl * albedo * sun_light_intensity + ambient, 0.0);
 }
