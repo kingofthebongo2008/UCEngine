53a54060d8480675d566315916bb398b05aeb227
lee dorian <leedorian88@abv.bg>
2017-10-24 21:12:21 +0300

    simple 4moment shadow maps, with code from the demos

--- src/uc_dev/private/gx/dx12/shaders/shadows/shadow_resolve.hlsl
+++ src/uc_dev/private/gx/dx12/shaders/shadows/shadow_resolve.hlsl
@@ -19,21 +19,43 @@ float p4(float x)
     return x * x * x * x;
 }
 
+/*! Given a scalar this function outputs a vector consisting of the first, second,
+third and fourth power of this scalar linearly transformed using an optimized
+basis for optimal quantization.*/
+float4 compute_moment_vector4_moments_optimized(float moment_0, float moment_1, float moment_2, float moment_3)
+{
+    const float4 moments        = float4(moment_0, moment_1, moment_2, moment_3);
+    const float4x4 transform    = float4x4
+        (
+            -2.07224649f,   13.7948857237f,     0.105877704f,   9.7924062118f,
+            32.23703778f,   -59.4683975703f,    -1.9077466311f, -33.7652110555f,
+            -68.571074599f, 82.0359750338f,     9.3496555107f,  47.9456096605f,
+            39.3703274134f, -35.364903257f,     -6.6543490743f, -23.9728048165f
+        );
+
+    float4 moments_optimized = moments;// mul(moments, transform);
+
+    //add bias
+    moments_optimized[0] += 0.035955884801f;
+
+    return moments;
+}
+
 [numthreads( 16, 16, 1 )]
 [RootSignature( MyRS2 ) ]
 void main( uint3 dtid : SV_DispatchThreadID )
 {
    const uint2 location = dtid.xy;
 
-   float z0 = g_shadows_buffer.Load(location,0);
-   float z1 = g_shadows_buffer.Load(location,1);
-   float z2 = g_shadows_buffer.Load(location,2);
-   float z3 = g_shadows_buffer.Load(location,3);
+   float z0 = 1.0f - g_shadows_buffer.Load(location,0);
+   float z1 = 1.0f - g_shadows_buffer.Load(location,1);
+   float z2 = 1.0f - g_shadows_buffer.Load(location,2);
+   float z3 = 1.0f - g_shadows_buffer.Load(location,3);
 
    float moment_0 = (z0 + z1 + z2 + z3) / 4;
    float moment_1 = (p2(z0) + p2(z1) + p2(z2) + p2(z3)) / 4;
    float moment_2 = (p3(z0) + p3(z1) + p3(z2) + p3(z3)) / 4;
    float moment_3 = (p4(z0) + p4(z1) + p4(z2) + p4(z3)) / 4;
 
-   g_shadow_moments[location] = float4( moment_0, moment_1, moment_2, moment_3);
+   g_shadow_moments[location] = compute_moment_vector4_moments_optimized( moment_0, moment_1, moment_2, moment_3);
 }
--- src/uc_dev/private/gx/dx12/shaders/static_geometry/textured_pixel_lit.hlsl
+++ src/uc_dev/private/gx/dx12/shaders/static_geometry/textured_pixel_lit.hlsl
@@ -19,6 +19,118 @@ cbuffer shadow_parameters : register(b2)
     float4                   m_light_direction;
 };
 
+
+/*! Given a sampled value from a four-moment shadow map and a computed shadow map
+depth for a point at the same location this function outputs 1.0, if the fragment
+is in shadow 0.0f, if the fragment is lit and an intermediate value for partial
+shadow. The returned value is an optimal lower bound.*/
+void Compute4MomentShadowIntensity(out float OutShadowIntensity, float4 _4Moments, float FragmentDepth, float DepthBias, float MomentBias)
+{
+    // Bias input data to avoid artifacts
+    float4 b = lerp(_4Moments, float4(0.5f, 0.5f, 0.5f, 0.5f), MomentBias);
+    float3 z;
+    z[0] = FragmentDepth - DepthBias;
+
+    // Compute a Cholesky factorization of the Hankel matrix B storing only non-
+    // trivial entries or related products
+    float L32D22 = mad(-b[0], b[1], b[2]);
+    float D22 = mad(-b[0], b[0], b[1]);
+    float SquaredDepthVariance = mad(-b[1], b[1], b[3]);
+    float D33D22 = dot(float2(SquaredDepthVariance, -L32D22), float2(D22, L32D22));
+    float InvD22 = 1.0f / D22;
+    float L32 = L32D22*InvD22;
+
+    // Obtain a scaled inverse image of bz=(1,z[0],z[0]*z[0])^T
+    float3 c = float3(1.0f, z[0], z[0] * z[0]);
+    // Forward substitution to solve L*c1=bz
+    c[1] -= b.x;
+    c[2] -= b.y + L32*c[1];
+    // Scaling to solve D*c2=c1
+    c[1] *= InvD22;
+    c[2] *= D22 / D33D22;
+    // Backward substitution to solve L^T*c3=c2
+    c[1] -= L32*c[2];
+    c[0] -= dot(c.yz, b.xy);
+    // Solve the quadratic equation c[0]+c[1]*z+c[2]*z^2 to obtain solutions z[1] 
+    // and z[2]
+    float p = c[1] / c[2];
+    float q = c[0] / c[2];
+    float D = ((p*p) / 4.0f) - q;
+    float r = sqrt(D);
+    z[1] = -(p / 2.0f) - r;
+    z[2] = -(p / 2.0f) + r;
+
+    // Use a solution made of four deltas if the solution with three deltas is invalid
+    if (z[1]<0.0f || z[2]>1.0f) {
+        float zFree = ((b[2] - b[1])*z[0] + b[2] - b[3]) / ((b[1] - b[0])*z[0] + b[1] - b[2]);
+        float w1Factor = (z[0]>zFree) ? 1.0f : 0.0f;
+        OutShadowIntensity = (b[1] - b[0] + (b[2] - b[0] - (zFree + 1.0f)*(b[1] - b[0]))*(zFree - w1Factor - z[0])
+            / (z[0] * (z[0] - zFree))) / (zFree - w1Factor) + 1.0f - b[0];
+    }
+    // Use the solution with three deltas
+    else {
+        float4 Switch =
+            (z[2]<z[0]) ? float4(z[1], z[0], 1.0f, 1.0f) : (
+            (z[1]<z[0]) ? float4(z[0], z[1], 0.0f, 1.0f) :
+                float4(0.0f, 0.0f, 0.0f, 0.0f));
+        float Quotient = (Switch[0] * z[2] - b[0] * (Switch[0] + z[2]) + b[1]) / ((z[2] - Switch[1])*(z[0] - z[1]));
+        OutShadowIntensity = Switch[2] + Switch[3] * Quotient;
+    }
+    OutShadowIntensity = saturate(OutShadowIntensity);
+}
+
+/*! Given a sampled value from a four-moment shadow map and a computed shadow map
+depth for a point at the same location this function outputs 1.0, if the fragment
+is in shadow 0.0f, if the fragment is lit and an intermediate value for partial
+shadow. The returned value is an optimal lower bound except for the fact that it
+does not exploit the knowledge that the original distribution has support in
+[0,1].*/
+void Compute4MomentUnboundedShadowIntensity(out float OutShadowIntensity,
+    float4 _4Moments, float FragmentDepth, float DepthBias, float MomentBias)
+{
+    // Bias input data to avoid artifacts
+    float4 b = lerp(_4Moments, float4(0.5f, 0.5f, 0.5f, 0.5f), MomentBias);
+    float3 z;
+    z[0] = FragmentDepth - DepthBias;
+
+    // Compute a Cholesky factorization of the Hankel matrix B storing only non-
+    // trivial entries or related products
+    float L32D22 = mad(-b[0], b[1], b[2]);
+    float D22 = mad(-b[0], b[0], b[1]);
+    float SquaredDepthVariance = mad(-b[1], b[1], b[3]);
+    float D33D22 = dot(float2(SquaredDepthVariance, -L32D22), float2(D22, L32D22));
+    float InvD22 = 1.0f / D22;
+    float L32 = L32D22*InvD22;
+
+    // Obtain a scaled inverse image of bz=(1,z[0],z[0]*z[0])^T
+    float3 c = float3(1.0f, z[0], z[0] * z[0]);
+    // Forward substitution to solve L*c1=bz
+    c[1] -= b.x;
+    c[2] -= b.y + L32*c[1];
+    // Scaling to solve D*c2=c1
+    c[1] *= InvD22;
+    c[2] *= D22 / D33D22;
+    // Backward substitution to solve L^T*c3=c2
+    c[1] -= L32*c[2];
+    c[0] -= dot(c.yz, b.xy);
+    // Solve the quadratic equation c[0]+c[1]*z+c[2]*z^2 to obtain solutions 
+    // z[1] and z[2]
+    float p = c[1] / c[2];
+    float q = c[0] / c[2];
+    float D = (p*p*0.25f) - q;
+    float r = sqrt(D);
+    z[1] = -p*0.5f - r;
+    z[2] = -p*0.5f + r;
+    // Compute the shadow intensity by summing the appropriate weights
+    float4 Switch =
+        (z[2]<z[0]) ? float4(z[1], z[0], 1.0f, 1.0f) : (
+        (z[1]<z[0]) ? float4(z[0], z[1], 0.0f, 1.0f) :
+            float4(0.0f, 0.0f, 0.0f, 0.0f));
+    float Quotient = (Switch[0] * z[2] - b[0] * (Switch[0] + z[2]) + b[1]) / ((z[2] - Switch[1])*(z[0] - z[1]));
+    OutShadowIntensity = Switch[2] + Switch[3] * Quotient;
+    OutShadowIntensity = saturate(OutShadowIntensity);
+}
+
 Texture2D<float4> g_shadow_moments         : register(t1);
 
 [RootSignature( MyRS1 ) ]
@@ -35,9 +147,25 @@ float4 main( interpolants r ) : SV_Target0
     light_ps                          = light_ps / light_ps.w;
     light_ps.xy                       = light_ps.xy * 0.5f + 0.5f;
     light_ps.y                        = 1.0f - light_ps.y;
+
+    float4 moments4                   = g_shadow_moments.Sample(g_linear_clamp, light_ps.xy).xyzw;
+    float  fragment_depth             = 1.0f - light_ps.z;
+    float  depth_bias                 = 0.00000f;
+    float  moment_bias                = 0.035955884801f;
+
+    float  shadow_intensity           = 0.0f;
+
+    Compute4MomentShadowIntensity(shadow_intensity, moments4, fragment_depth, depth_bias, moment_bias);
+
+    shadow_intensity = 1.0f - shadow_intensity;
+    /*
+
     float  shadow                     = g_shadow_moments.Sample(g_linear_clamp,  light_ps.xy ).x;
 
     float  shadow_intensity           = shadow > light_ps.z ? 0.0f : 1.0f; 
+
+    */
+
     float  ndotl                      = saturate(dot(normal_ws, sun_light_direction_ws));
     float3 ambient                    = float3(0.2f, 0.2f, 0.2f);
     
