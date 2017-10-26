80dcabf9b70275346a48676f3ccab7fb5e541cc9
lee dorian <leedorian88@abv.bg>
2017-10-26 09:53:59 +0300

    pulled out resolve into custom function

--- src/uc_dev/private/gx/dx12/shaders/shadows/shadow_resolve.hlsl
+++ src/uc_dev/private/gx/dx12/shaders/shadows/shadow_resolve.hlsl
@@ -234,29 +234,31 @@ void ApplyResolveAndGaussian8_64Bit(inout RWTexture2D<unorm float4> OutMomentSha
     }
 }
 
+void ApplyResolve_64Bit(inout RWTexture2D<unorm float4> shadow_moments, Texture2DMS<float, 4> shadows_buffer, uint3 dtid : SV_DispatchThreadID)
+{
+   
+    const uint2 location = dtid.xy;
+
+    float z0 = 1.0f - shadows_buffer.Load(location,0);
+    float z1 = 1.0f - shadows_buffer.Load(location,1);
+    float z2 = 1.0f - shadows_buffer.Load(location,2);
+    float z3 = 1.0f - shadows_buffer.Load(location,3);
+
+    float moment_0 = (z0 + z1 + z2 + z3) / 4;
+    float moment_1 = (p2(z0) + p2(z1) + p2(z2) + p2(z3)) / 4;
+    float moment_2 = (p3(z0) + p3(z1) + p3(z2) + p3(z3)) / 4;
+    float moment_3 = (p4(z0) + p4(z1) + p4(z2) + p4(z3)) / 4;
+
+    shadow_moments[location] = compute_moment_vector4_moments_optimized( moment_0, moment_1, moment_2, moment_3);
+}
+
 
 Texture2DMS<float, 4>        g_shadows_buffer:register(t1);
 RWTexture2D<unorm float4>    g_shadow_moments:register(u1);
-
 
 [numthreads( 32, 8, 1 )]
 [RootSignature( MyRS2 ) ]
 void main( uint3 dtid : SV_DispatchThreadID, uint3 GroupThreadID : SV_GroupThreadID, uint3 GroupID : SV_GroupID)
 {
-   /*
-   const uint2 location = dtid.xy;
-
-   float z0 = 1.0f - g_shadows_buffer.Load(location,0);
-   float z1 = 1.0f - g_shadows_buffer.Load(location,1);
-   float z2 = 1.0f - g_shadows_buffer.Load(location,2);
-   float z3 = 1.0f - g_shadows_buffer.Load(location,3);
-
-   float moment_0 = (z0 + z1 + z2 + z3) / 4;
-   float moment_1 = (p2(z0) + p2(z1) + p2(z2) + p2(z3)) / 4;
-   float moment_2 = (p3(z0) + p3(z1) + p3(z2) + p3(z3)) / 4;
-   float moment_3 = (p4(z0) + p4(z1) + p4(z2) + p4(z3)) / 4;
-
-   g_shadow_moments[location] = compute_moment_vector4_moments_optimized( moment_0, moment_1, moment_2, moment_3);
-   */
    ApplyResolveAndGaussian8_64Bit(g_shadow_moments, g_shadows_buffer, GroupThreadID, GroupID);
 }
