4af980463ee8ab0b35261080b0fcaff43c0d1cc6
lee dorian <leedorian88@abv.bg>
2018-01-07 12:38:29 +0200

    added bat file for the ucdev project

---
+++ build/ucdev.bat
@@ -0,0 +1,47 @@
+@echo off
+
+
+echo ^<?xml version="1.0" encoding="utf-8"?^> > ucdev_app_h.msbuild
+echo ^<Project DefaultTargets="Build" ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003"^> >> ucdev_app_h.msbuild
+echo ^<ItemGroup^> >> ucdev_app_h.msbuild
+
+ucdev_cpp_source_generator_r.exe --input ..\src\app\ --mode h >> ucdev_app_h.msbuild
+ucdev_cpp_source_generator_r.exe --input ..\src\uc_dev\ --mode h >> ucdev_app_h.msbuild
+ucdev_cpp_source_generator_r.exe --input ..\include\uc_dev\ --mode h >> ucdev_app_h.msbuild
+
+echo ^</ItemGroup^> >> ucdev_app_h.msbuild
+echo ^</Project^> >> ucdev_app_h.msbuild
+
+echo ^<?xml version="1.0" encoding="utf-8"?^> > ucdev_app_cpp.msbuild
+echo ^<Project DefaultTargets="Build" ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003"^> >> ucdev_app_cpp.msbuild
+echo ^<ItemGroup^> >> ucdev_app_cpp.msbuild
+
+ucdev_cpp_source_generator_r.exe --input ..\src\app\ --mode cpp >> ucdev_app_cpp.msbuild
+ucdev_cpp_source_generator_r.exe --input ..\src\uc_dev\ --mode cpp    >> ucdev_app_cpp.msbuild
+
+echo ^</ItemGroup^> >> ucdev_app_cpp.msbuild
+echo ^</Project^> >> ucdev_app_cpp.msbuild
+
+
+echo ^<?xml version="1.0" encoding="utf-8"?^> > ucdev_app_hlsl.msbuild
+echo ^<Project DefaultTargets="Build" ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003"^> >> ucdev_app_hlsl.msbuild
+echo ^<ItemGroup^> >> ucdev_app_hlsl.msbuild
+
+ucdev_cpp_source_generator_r.exe --input ..\src\uc_dev\private\gx\dx12\shaders\ --mode hlsl >> ucdev_app_hlsl.msbuild
+
+echo ^</ItemGroup^> >> ucdev_app_hlsl.msbuild
+echo ^</Project^> >> ucdev_app_hlsl.msbuild
+
+
+echo ^<?xml version="1.0" encoding="utf-8"?^> > ucdev_app_pso.msbuild
+echo ^<Project DefaultTargets="Build" ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003"^> >> ucdev_app_pso.msbuild
+echo ^<ItemGroup^> >> ucdev_app_pso.msbuild
+
+ucdev_cpp_source_generator_r.exe --input ..\src\uc_dev\private\gx\dx12\shaders\ --mode pso >> ucdev_app_pso.msbuild
+
+echo ^</ItemGroup^> >> ucdev_app_pso.msbuild
+echo ^</Project^> >> ucdev_app_pso.msbuild
+
+
+
+
