38a796e5813a74a9c5c26a78eafb8e974c082f3d
lee dorian <leedorian89@abv.bg>
2018-03-17 18:45:58 +0200

    made renderworld2 to match military mechanic

--- src/app/private/uwp/uc_uwp_gx_render_world_2.cpp
+++ src/app/private/uwp/uc_uwp_gx_render_world_2.cpp
@@ -6,6 +6,7 @@
 #include <array>
 
 #include <uc_dev/gx/dx12/gpu/texture_2d.h>
+#include <uc_dev/gx/dx12/cmd/profiler.h>
 
 #include <uc_dev/gx/lip/file.h>
 #include <uc_dev/gx/lip_utils.h>
@@ -19,53 +20,20 @@
 #include "uc_uwp_gx_render_object_factory.h"
 #include "uc_uwp_device_resources.h"
 
+#include <autogen/shaders/textured_skinned_lit_material_graphics.h>
+#include <autogen/shaders/textured_skinned_lit_depth_only_graphics.h>
+#include <autogen/shaders/textured_skinned_lit_shadows_graphics.h>
+#include <autogen/shaders/shadows_resolve_compute.h>
+#include <autogen/shaders/plane_solid_graphics.h>
+#include <autogen/shaders/plane_depth_only_graphics.h>
+#include <autogen/shaders/plane_shadows_graphics.h>
+
 namespace uc
 {
     namespace uwp
     {
         namespace gxu
         {
-            namespace details
-            {
-                struct skinned_draw_constants
-                {
-                    math::float4x4                  m_world;
-                    std::array<math::float4x4, 127> m_joints_palette;
-
-                    skinned_draw_constants()
-                    {
-                        m_world = math::identity_matrix();
-
-                        for (auto&& i : m_joints_palette)
-                        {
-                            i = math::identity_matrix();
-                        }
-                    }
-                };
-
-                inline math::float4x4 military_mechanic_world_transform(double frame_time)
-                {
-                    //todo: move this in the update pahse
-                    const float total = 3.14159265358979323846f * 2.0f;
-                    const  uint32_t steps = 144;
-
-                    const float step_rotation = total / steps;
-                    static uint32_t step = 0;
-
-                    static float angle = 0.0f;
-
-                    //const float rotation_angle = (step)* step_rotation;
-
-                    const float rotation_angle = static_cast<float>(angle + 100.0f * frame_time * step_rotation);
-                    angle = rotation_angle;
-
-                    math::float4x4 t0 = math::translation(-8.0f, 15.0f, 0.0f);
-                    math::float4x4 t1 = math::rotation_y(rotation_angle);
-                    math::float4x4 t2 = math::translation(-30.0f, -15.0f, 0.0f);
-
-                    return t1;// math::mul(t1, t0);
-                }
-            }
 
             render_world_2::render_world_2(initialize_context* c) : base(c)
             {
@@ -73,42 +41,29 @@ namespace uc
 
                 g.run([this, c]
                 {
-                    m_military_mechanic = gxu::make_render_object_from_file<skinned_render_object>(L"appdata/meshes/military_mechanic.skinned.model", c->m_resources, c->m_geometry);
+                    auto resources = c->m_resources;
+                    m_skinned = gx::dx12::create_pso(resources->device_d2d12(), resources->resource_create_context(), gx::dx12::textured_skinned_lit_material_graphics::create_pso);
                 });
 
                 g.run([this, c]
                 {
                     auto resources = c->m_resources;
-                    m_textured_skinned = gx::dx12::create_pso(resources->device_d2d12(), resources->resource_create_context(), gx::dx12::textured_skinned_solid_graphics::create_pso);
+                    m_plane = gx::dx12::create_pso(resources->device_d2d12(), resources->resource_create_context(), gx::dx12::plane_solid_graphics::create_pso);
                 });
+
 
                 g.run([this, c]
                 {
                     auto resources = c->m_resources;
-                    m_skeleton_pso = gx::dx12::create_pso(resources->device_d2d12(), resources->resource_create_context(), gx::dx12::skeleton_graphics::create_pso);
+                    m_shadows_resolve = gx::dx12::create_pso(resources->device_d2d12(), resources->resource_create_context(), gx::dx12::shadows_resolve_compute::create_pso);
                 });
 
+
+                //load blue noise
                 g.run([this, c]
                 {
-                    auto resources = c->m_resources;
-                    m_textured_skinned_depth = gx::dx12::create_pso(resources->device_d2d12(), resources->resource_create_context(), gx::dx12::textured_skinned_depth_only_graphics::create_pso);
+                    m_blue_noise = gx::blue_noise::make_blue_noise(c->m_resources->resource_create_context(), c->m_resources->upload_queue());
                 });
-
-                //load preprocessed textured model
-                g.run([this]()
-                {
-                    m_military_mechanic_skeleton    = lip::create_from_compressed_lip_file<lip::skeleton>(L"appdata/skeletons/military_mechanic.skeleton");
-                    m_skeleton_instance             = std::make_unique<gx::anm::skeleton_instance>(m_military_mechanic_skeleton.get());
-                });
-
-                //load preprocessed textured model
-                g.run([this]()
-                {
-                    m_military_mechanic_animations = lip::create_from_compressed_lip_file<lip::joint_animations>(L"appdata/animations/military_mechanic.animation");
-                });
-
-                m_camera->set_view_position(math::set(0.0, 0.0f, -5.5f, 0.0f));
-                m_camera->set_far(1200.0f);
 
                 g.wait();
 
@@ -123,11 +78,89 @@ namespace uc
 
             void render_world_2::do_update(update_context* ctx)
             {
-                *m_military_mechanic_transform = details::military_mechanic_world_transform(ctx->m_frame_time);
-
-                m_skeleton_instance->reset();
-                m_animation_instance->accumulate(   m_skeleton_instance.get(), ctx->m_frame_time    );
+                base::do_update(ctx);
                 m_skeleton_positions = gx::anm::skeleton_positions(m_military_mechanic_skeleton.get(), m_skeleton_instance->local_transforms());
+            }
+
+            std::unique_ptr< submitable > render_world_2::do_render_shadows(shadow_render_context* ctx)
+            {
+                //now start new ones
+                auto resources = ctx->m_resources;
+                auto graphics = create_graphics_compute_command_context(resources->direct_command_context_allocator(device_resources::swap_chains::background));
+
+
+                {
+                    auto profile_event = uc::gx::dx12::make_profile_event(graphics.get(), L"Shadows Pass");
+
+                    graphics->clear_depth(ctx->m_shadow_depth_buffer, 0.0f);
+                    graphics->set_render_target(ctx->m_shadow_depth_buffer);
+
+                    {
+                        set_view_port(ctx, graphics.get());
+                    }
+
+                    //Per many draw calls  -> frequency 1
+                    graphics->set_primitive_topology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+                    graphics->set_pso(m_skinned_shadows);
+                    graphics->set_descriptor_heaps();
+
+                    graphics->set_graphics_constant_buffer(gx::dx12::default_root_singature::slots::constant_buffer_0, m_constants_frame_shadows);
+
+                    //mechanic
+                    {
+                        auto profile_event0 = uc::gx::dx12::make_profile_event(graphics.get(), L"Mechanic");
+
+                        //todo: move this into a big buffer for the whole scene
+                        graphics->set_graphics_dynamic_constant_buffer(gx::dx12::default_root_singature::slots::constant_buffer_1, 0, m_constants_pass);
+
+                        //geometry
+                        graphics->set_vertex_buffer(0, ctx->m_geometry->skinned_mesh_position_view());
+                        graphics->set_vertex_buffer(1, ctx->m_geometry->skinned_mesh_blend_weight_view());
+                        graphics->set_vertex_buffer(2, ctx->m_geometry->skinned_mesh_blend_index_view());
+                        graphics->set_index_buffer(ctx->m_geometry->indices_view());
+
+                        //Draw call -> frequency 2 ( nvidia take care these should be on a sub 1 ms granularity)
+                        graphics->draw_indexed(m_military_mechanic->m_indices->index_count(), m_military_mechanic->m_indices->index_offset(), m_military_mechanic->m_geometry->draw_offset());
+                    }
+
+                    //plane
+                    {
+                        auto profile_event0 = uc::gx::dx12::make_profile_event(graphics.get(), L"Plane");
+
+                        math::float4x4 m = math::identity_matrix();
+
+                        graphics->set_pso(m_plane_shadows);
+
+                        graphics->set_graphics_dynamic_constant_buffer(gx::dx12::default_root_singature::slots::constant_buffer_1, 0, m);
+
+                        gx::dx12::graphics_reset_vertex_buffer(graphics, 0);
+                        gx::dx12::graphics_reset_index_buffer(graphics);
+                        graphics->draw(6);
+                    }
+
+                    end_render_shadows(ctx, graphics.get());
+                }
+
+                {
+
+                    graphics->transition_resource(ctx->m_shadow_depth_buffer, D3D12_RESOURCE_STATE_DEPTH_WRITE, D3D12_RESOURCE_STATE_DEPTH_READ | D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE);
+
+                    {
+                        auto profile_event = uc::gx::dx12::make_profile_event(graphics.get(), L"Shadows Resolve Pass");
+
+                        graphics->set_pso(m_shadows_resolve);
+
+                        graphics->set_compute_dynamic_descriptor(gx::dx12::default_root_singature::slots::srv_1, ctx->m_shadow_depth_buffer->srv_depth());
+                        graphics->set_compute_dynamic_descriptor(gx::dx12::default_root_singature::slots::uav_1, ctx->m_shadow_map->uav());
+
+                        graphics->dispatch(2048 / 32, 2048 / 8, 1);
+
+                    }
+
+                    graphics->transition_resource(ctx->m_shadow_map, D3D12_RESOURCE_STATE_UNORDERED_ACCESS, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE | D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE);
+                }
+
+                return std::make_unique<graphics_compute_submitable>(std::move(graphics));
             }
 
             std::unique_ptr< submitable >render_world_2::do_render(render_context* ctx)
@@ -135,6 +168,8 @@ namespace uc
                 //now start new ones
                 auto resources = ctx->m_resources;
                 auto graphics = create_graphics_command_context(resources->direct_command_context_allocator(device_resources::swap_chains::background));
+
+                auto profile_event = uc::gx::dx12::make_profile_event(graphics.get(), L"Main Pass");
 
                 begin_render(ctx, graphics.get());
 
@@ -145,43 +180,38 @@ namespace uc
 
                 //Per many draw calls  -> frequency 1
                 graphics->set_primitive_topology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-                graphics->set_pso(m_textured_skinned);
+                graphics->set_pso(m_skinned);
+                graphics->set_descriptor_heaps();
 
+                graphics->set_graphics_constant_buffer(gx::dx12::default_root_singature::slots::constant_buffer_0, m_constants_frame);
+
+                //shadows and light
                 {
-                    frame_constants frame;
+                    struct render_object_frame_constants
+                    {
 
-                    frame.m_view        = uc::math::transpose(uc::gx::view_matrix(camera()));
-                    frame.m_perspective = uc::math::transpose(uc::gx::perspective_matrix(camera()));
-                    graphics->set_graphics_constant_buffer(gx::dx12::default_root_singature::slots::constant_buffer_0, frame);
+                        math::float4x4 m_shadow_view;
+                        math::float4x4 m_shadow_perspective;
+                        math::float4   m_directional_light;
+                    };
+
+                    render_object_frame_constants r;
+
+                    r.m_shadow_view = m_constants_frame_shadows.m_view;
+                    r.m_shadow_perspective = m_constants_frame_shadows.m_perspective;
+                    r.m_directional_light = m_light_direction;
+                    graphics->set_graphics_dynamic_constant_buffer(gx::dx12::default_root_singature::slots::constant_buffer_1, 1, r);
                 }
 
-                //robot
+                graphics->set_graphics_dynamic_descriptor(gx::dx12::default_root_singature::slots::srv_1, ctx->m_shadow_map->srv(), 0);
+                graphics->set_graphics_dynamic_descriptor(gx::dx12::default_root_singature::slots::srv_1, m_blue_noise->srv(), 1);
+
+
+                //mechanic
                 {
-                    //draw
-                    details::skinned_draw_constants draw;
-                    draw.m_world = math::identity_matrix(); //uc::math::transpose(m_military_mechanic_transform); //// 
-
-                    {
-                        auto skeleton = m_military_mechanic_skeleton.get();
-                        auto joints   = gx::anm::local_to_world_joints2( skeleton, m_skeleton_instance->local_transforms() );
-                        
-                        for (auto i = 0U; i < joints.size(); ++i )
-                        {
-                            math::float4x4 bind_pose = math::load44(&skeleton->m_joint_inverse_bind_pose2[i].m_a0);
-                            math::float4x4 palette   = math::mul(bind_pose, joints[i]  ); // math::mul(bind_pose, joints[i]);
-
-                           
-                            draw.m_joints_palette[i] = math::transpose(palette);
-                            //draw.m_joints_palette[i] = math::transpose(math::mul(math::swap_y_z(), palette));
-                        }
-                    }
-
-                    graphics->set_pso(m_textured_skinned);
-                    graphics->set_descriptor_heaps();
-
+                    auto profile_event0 = uc::gx::dx12::make_profile_event(graphics.get(), L"Mechanic");
                     //todo: move this into a big buffer for the whole scene
-                    graphics->set_graphics_dynamic_constant_buffer(gx::dx12::default_root_singature::slots::constant_buffer_1, 0, draw);
-                    graphics->set_primitive_topology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+                    graphics->set_graphics_dynamic_constant_buffer(gx::dx12::default_root_singature::slots::constant_buffer_1, 0, m_constants_pass);
 
                     //geometry
                     graphics->set_vertex_buffer(0, ctx->m_geometry->skinned_mesh_position_view());
@@ -191,19 +221,19 @@ namespace uc
                     graphics->set_vertex_buffer(4, ctx->m_geometry->skinned_mesh_blend_index_view());
                     graphics->set_index_buffer(ctx->m_geometry->indices_view());
 
-                    size_t start    = 0;
-                    size_t size     = m_military_mechanic->m_primitive_ranges.size();
+                    size_t start = 0;
+                    size_t size = m_military_mechanic->m_primitive_ranges.size();
 
-                    for (auto j = start; j < start + size; ++j)
+                    for (auto i = start; i < start + size; ++i)
                     {
                         {
-                            auto& t = m_military_mechanic->m_opaque_textures[j];
+                            //auto& t = m_military_mechanic->m_opaque_textures[i];
                             //material
-                            graphics->set_graphics_dynamic_descriptor(gx::dx12::default_root_singature::slots::srv_1, t->srv());
+
                         }
 
                         {
-                            auto& r = m_military_mechanic->m_primitive_ranges[j];
+                            auto& r = m_military_mechanic->m_primitive_ranges[i];
                             auto  base_index_offset = m_military_mechanic->m_indices->index_offset();
                             auto  base_vertex_offset = m_military_mechanic->m_geometry->draw_offset();
 
@@ -213,29 +243,21 @@ namespace uc
                     }
                 }
 
+
+                //plane
                 {
-                    //Per many draw calls  -> frequency 1
-                    graphics->set_primitive_topology(D3D_PRIMITIVE_TOPOLOGY_LINELIST);
-                    graphics->set_pso(m_skeleton_pso);
+                    auto profile_event0 = uc::gx::dx12::make_profile_event(graphics.get(), L"Plane");
 
-                    {
-                        frame_constants frame;
-                        frame.m_view = uc::math::transpose(uc::gx::view_matrix(camera()));
-                        frame.m_perspective = uc::math::transpose(uc::gx::perspective_matrix(camera()));
-                        graphics->set_graphics_constant_buffer(gx::dx12::default_root_singature::slots::constant_buffer_0, frame);
-                    }
+                    math::float4x4 m = math::identity_matrix();
 
-                    {
-                        //draw
-                        draw_constants draw;
-                        draw.m_world = uc::math::identity_matrix();// uc::math::transpose(m_skeleton_positions_transform);
+                    graphics->set_pso(m_plane);
 
-                        //todo: move this into a big buffer for the whole scene
-                        graphics->set_graphics_dynamic_constant_buffer(gx::dx12::default_root_singature::slots::constant_buffer_1, 0, draw);
+                    graphics->set_graphics_dynamic_constant_buffer(gx::dx12::default_root_singature::slots::constant_buffer_1, 0, m);
 
-                        graphics->set_dynamic_vertex_buffer(0, sizeof(gx::position_3d), resources->upload_queue()->upload_buffer(&m_skeleton_positions[0], m_skeleton_positions.size() * sizeof(gx::position_3d)));
-                        graphics->draw(static_cast<uint32_t>(m_skeleton_positions.size()));
-                    }
+                    gx::dx12::graphics_reset_vertex_buffer(graphics, 0);
+                    gx::dx12::graphics_reset_index_buffer(graphics);
+
+                    graphics->draw(6);
                 }
 
                 end_render(ctx, graphics.get());
--- src/app/private/uwp/uc_uwp_gx_render_world_2.h
+++ src/app/private/uwp/uc_uwp_gx_render_world_2.h
@@ -8,8 +8,9 @@
 #include <uc_dev/gx/geo/indexed_geometry.h>
 #include <uc_dev/gx/anm/animation_instance.h>
 #include <uc_dev/gx/structs.h>
+#include <uc_dev/gx/blue_noise/moment_shadow_maps_blue_noise.h>
 
-#include "uc_uwp_gx_render_world.h"
+#include "uc_uwp_gx_render_world_moment_shadows_data.h"
 
 namespace uc
 {
@@ -17,10 +18,9 @@ namespace uc
     {
         namespace gxu
         {
-            class render_world_2 : public render_world
+            class render_world_2 : public render_world_moment_shadows_data
             {
-
-                using base = render_world;
+                using base = render_world_moment_shadows_data;
 
             public:
 
@@ -31,23 +31,11 @@ namespace uc
 
                 void do_update(update_context* ctx) override;
                 std::unique_ptr< submitable > do_render(render_context* ctx) override;
-                
-                std::unique_ptr<gxu::skinned_render_object>                     m_military_mechanic;
-
-                gx::dx12::graphics_pipeline_state*                              m_textured_skinned;
-                gx::dx12::graphics_pipeline_state*                              m_textured_skinned_depth;
-
-                lip::unique_lip_pointer<lip::skeleton>                          m_military_mechanic_skeleton;
-                lip::unique_lip_pointer<lip::joint_animations>                  m_military_mechanic_animations;
-
-                std::unique_ptr< gx::anm::skeleton_instance >                   m_skeleton_instance;
-                std::unique_ptr< gx::anm::animation_instance>                   m_animation_instance;
-
-                //update state
-                math::managed_float4x4                                          m_military_mechanic_transform = math::make_float4x4();
+                std::unique_ptr< submitable > do_render_shadows(shadow_render_context* ctx) override;
 
                 gx::dx12::graphics_pipeline_state*                              m_skeleton_pso;
                 std::vector<gx::position_3d>                                    m_skeleton_positions;
+                std::unique_ptr<gx::blue_noise::ldr_rg01_64x64>                 m_blue_noise;
             };
         }
     }
