cd28b9fc3a1dfebe499794ed6067c8917d72fe34
lee dorian <leedorian89@abv.bg>
2018-04-07 15:13:51 +0300

    remove old stuff

--- build/run.bat
+++ build/run.bat
@@ -1,6 +1,6 @@
 rem msbuild uc_engine.vcxproj /t:Build /p:Configuration=release /p:Platform=x64 /verbosity:detailed
 rem msbuild ucdev_app.vcxproj /t:Clean /p:Configuration=release /p:Platform=x64 /verbosity:detailed
-msbuild ucdev_app.vcxproj /t:Clean /p:Configuration=debug /p:Platform=x64 /verbosity:detailed /p:DeployOnBuild=true
-rem msbuild /t:Build /p:Configuration=debug /p:Platform=x64 /verbosity:detailed /p:DeployOnBuild=true ucdev_app.vcxproj
+rem msbuild ucdev_app.vcxproj /t:Clean /p:Configuration=debug /p:Platform=x64 /verbosity:detailed /p:DeployOnBuild=true
+msbuild /t:Build /p:Configuration=debug /p:Platform=x64 /verbosity:detailed /p:DeployOnBuild=true ucdev_app.vcxproj
 
 
